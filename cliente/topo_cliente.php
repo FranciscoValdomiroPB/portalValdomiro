dda4dff18c86b796d54b1599471fdcf872e373ad
Francisco valdomiro <valdopinto.gmail.com>
2023-05-16 19:10:24 -0400

    tirei o que estava destroindo a sessao do cliente

--- cliente/topo_cliente.php
+++ cliente/topo_cliente.php
@@ -12,23 +12,6 @@ if (!isset($_SESSION['nome_login'])) {
 	echo "<script> window.location.href='http://localhost/portalValdomiro';</script>";
 
 }
-
-if ($_SESSION['tipo_login'] <> 0) {
-
-	echo "<script> alert('ERRO: VOCÊ NÃO TEM PERMISSÃO PARA ACESSAR ESTA PÁGINA!');</script>";
-	session_destroy();
-
-	unset($_SESSION['nome_completo_login']);
-	unset($_SESSION['nome_login']);
-	unset($_SESSION['tipo_login']);
-
-	unset($_SESSION['url']);
-	unset($_SESSION['url_admin']);
-	unset($_SESSION['url_cliente']);
-
-	echo "<script> window.location.href='http://localhost/portalValdomiro';</script>";
-
-}
 ?>
 
 <!DOCTYPE html>
