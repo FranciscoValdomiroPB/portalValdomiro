f3a30279beeba8549bf359c96fa4abd17267c100
Francisco valdomiro <valdopinto.gmail.com>
2023-05-24 18:33:48 -0400

    atualização

--- cliente/deliverys/deliverys.php
+++ cliente/deliverys/deliverys.php
@@ -1,50 +1,41 @@
-<?php require('../topo_cliente.php'); ?>
+<?php require('../topo_cliente.php');
+require('../../conexao.php'); ?>
 <!-- ARQUIVO DE ESTILO DO PORTAL -->
-<link rel="stylesheet" type="text/css" href="http://localhost//portalValdomiro/css/estilo.css">
+<link rel="stylesheet" type="text/css" href="http://localhost//portalValdomiro/css/estiloCliente.css">
 
 <!--CONTEÚDO DA PÁGINA DELIVERY---->
 <main>
-    <section>
-        <h2>Nossos Restaurantes</h2> 
-        <div class="reyaurantes">
-            <div class="restaurante">
-                <div class="imgRestaurantes"><img
-                        src="https://lh5.googleusercontent.com/p/AF1QipO2jDlPIaR7yBzefs9mbiuQ6Zslf_vcIsukft6V=w408-h291-k-no">
-                </div>
-                <h3>Restaurante Papaleguas</h3>
-                <ul>
-                    <li><a href="https://wa.me/9299268-9498">Whatsapp</a></li>
-                    <li><a href="https://goo.gl/maps/9GyK5uSzKMZziEKk7">localização</a></li>
-                </ul>
-                <p></p>
-            </div>
-            <div class="restaurante">
-                <div class="imgRestaurantes"><img
-                        src="https://lh5.googleusercontent.com/p/AF1QipOadJTyhgEoygT29P48CWHP8MFwijIZKZX1b_Re=w203-h152-k-no">
-                </div>
-                <h3>Chapa Quente</h3>
-                <ul>
-                    <li><a href="https://wa.me/9299117-6669">Whatsapp</a></li>
-                    <li><a href="https://goo.gl/maps/mrv3tTMhhRKfu6aM9">localização</a></li>
-                </ul>
-                <p></p>
-            </div>
-            <div class="restaurante">
-                <div class="imgRestaurantes"><img
-                        src="https://lh5.googleusercontent.com/p/AF1QipOVh1wYHWOBAM_S2_E-4wVUHyL5Bsx_5pSgD-sx=w408-h306-k-no">
-                </div>
-                <h3>Bar E Restaurante Xodó Da Cidade</h3>
-                <ul>
-                    <li><a href="https://wa.me/09233240805">Whatsapp</a></li>
-                    <li><a href="https://goo.gl/maps/yjRXb5suzL5U6oZZA">localização</a></li>
-                </ul>
-                <p></p>
-            </div>
-        </div>
-    </section>
+	<section>
+		<div class="pousadas">
+			<?php
+
+			// Consulta para obter as informações das pousadas
+			$result = mysqli_query($conexao, "SELECT * FROM restaurante");
+
+			// Verifica se há registros
+			if (mysqli_num_rows($result) > 0) {
+				// Itera sobre os resultados e exibe as pousadas na vitrine
+				while ($row = mysqli_fetch_assoc($result)) {
+					echo '<div class="pousada">';
+					echo '<img src="' . $row['fotos'] . '" alt="' . $row['nome'] . '">';
+					echo '<h3>' . $row['nome'] . '</h3>';
+					echo '<p>' . $row['descricao'] . '</p>';
+					echo '<p><a href="' . $row['endereço_link'] . '">Contato: ' . $row['contato'] .'</a></p>';
+					echo '<p><a href="' . $row['endereço_link'] . '">Endereço: ' . $row['endereco'] .'</a></p>';
+					echo '</div>';
+				}
+			} else {
+				echo '<p>Não existem restaurantes cadastrados.</p>';
+			}
+
+			// Fecha a conexão com o banco de dados
+			mysqli_close($conexao);
+			?>
+		</div>
+	</section>
 </main>
 <footer>
-    <p>Francisco Valdomiro © 2023</p>
+	<p>GRUTA © 2023</p>
 </footer>
 </body>
 
